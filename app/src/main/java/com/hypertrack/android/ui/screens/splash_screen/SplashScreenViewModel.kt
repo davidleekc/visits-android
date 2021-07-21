ec07abe269bf24af62e3892827e7d5513de2f056
pavel-kuzntesov-5x <pavel.kuzntesov.5x@gmail.com>
2021-07-21 15:00:12 +0300

    Deeplink error handling

--- app/src/main/java/com/hypertrack/android/ui/screens/splash_screen/SplashScreenViewModel.kt
+++ app/src/main/java/com/hypertrack/android/ui/screens/splash_screen/SplashScreenViewModel.kt
@@ -1,10 +1,8 @@
 package com.hypertrack.android.ui.screens.splash_screen
 
 import android.app.Activity
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
-import com.google.gson.Gson
 import com.hypertrack.android.interactors.PermissionDestination
 import com.hypertrack.android.interactors.PermissionsInteractor
 import com.hypertrack.android.repository.AccountRepository
@@ -13,6 +11,7 @@ import com.hypertrack.android.ui.base.BaseViewModel
 import com.hypertrack.android.ui.common.isEmail
 import com.hypertrack.android.utils.CrashReportsProvider
 import com.hypertrack.android.utils.OsUtilsProvider
+import com.hypertrack.android.utils.format
 import com.hypertrack.logistics.android.github.R
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.Types
@@ -32,27 +31,10 @@ class SplashScreenViewModel(
     fun handleDeeplink(parameters: Map<String, Any>, activity: Activity) {
 //        Log.v("hypertrack-verbose", parameters.toString())
         if (parameters.isNotEmpty()) {
-            val key = parameters["publishable_key"] as String?
-            val driverId = parameters["driver_id"] as String?
-            val email = parameters["email"] as String?
-            val deeplink = parameters["~referring_link"] as String?
-            val deeplinkWithoutGetParams = deeplink.urlClearGetParams()
-            val phoneNumber = parameters["phone_number"] as String?
-            val metadata: Map<String, Any>? = try {
-                val param = parameters["metadata"]
-                when (param) {
-                    is String -> {
-                        moshi.adapter<Map<String, Any>>(
-                            Types.newParameterizedType(
-                                Map::class.java, String::class.java,
-                                Any::class.java
-                            )
-                        ).fromJson(param)
-                    }
-                    else -> null
-                }
+            crashReportsProvider.log("Got deeplink: ${parameters}")
+            try {
+                parseDeeplink(parameters, activity)
             } catch (e: Exception) {
-                crashReportsProvider.logException(e)
                 errorHandler.postText(
                     osUtilsProvider.stringFromResource(
                         R.string.splash_screen_invalid_link,
@@ -62,83 +44,130 @@ class SplashScreenViewModel(
                         )
                     )
                 )
+                crashReportsProvider.logException(InvalidDeeplinkException(e))
                 proceedToSignUp()
-                return
-            }
-
-            when {
-                key == null -> {
-                    errorHandler.postText(
-                        osUtilsProvider.stringFromResource(
-                            R.string.splash_screen_invalid_link,
-                            osUtilsProvider.stringFromResource(R.string.splash_screen_no_key)
-                        )
-                    )
-                    proceedToSignUp()
-                }
-                email == null && phoneNumber == null && driverId == null -> {
-                    errorHandler.postText(
-                        osUtilsProvider.stringFromResource(
-                            R.string.splash_screen_invalid_link,
-                            osUtilsProvider.stringFromResource(R.string.splash_screen_no_username)
-                        )
-                    )
-                    proceedToSignUp()
-                }
-                else -> {
-                    loadingState.postValue(true)
-                    viewModelScope.launch {
-                        try {
-                            val correctKey = accountRepository.onKeyReceived(key)
-                            // Log.d(TAG, "onKeyReceived finished")
-                            if (correctKey) {
-                                // Log.d(TAG, "Key validated successfully")
-                                if (driverId != null && (email == null && phoneNumber == null)) {
-                                    errorHandler.postText(
-                                        osUtilsProvider.stringFromResource(
-                                            R.string.splash_screen_deprecated_link
-                                        )
-                                    )
-                                    if (driverId.isEmail()) {
-                                        driverRepository.setUserData(
-                                            email = driverId,
-                                            phoneNumber = phoneNumber,
-                                            metadata = metadata,
-                                            deeplinkWithoutGetParams = deeplinkWithoutGetParams
-                                        )
-                                    } else {
-                                        driverRepository.setUserData(
-                                            driverId = driverId,
-                                            phoneNumber = phoneNumber,
-                                            metadata = metadata,
-                                            deeplinkWithoutGetParams = deeplinkWithoutGetParams
-                                        )
-                                    }
-                                    proceedToVisitsManagement(activity)
-                                } else {
-                                    driverRepository.setUserData(
-                                        email = email,
-                                        phoneNumber = phoneNumber,
-                                        metadata = metadata,
-                                        deeplinkWithoutGetParams = deeplinkWithoutGetParams
-                                    )
-                                    proceedToVisitsManagement(activity)
-                                }
-                            } else {
-                                throw Exception("Invalid publishable_key")
-                            }
-                        } catch (e: Exception) {
-                            Log.w(TAG, "Cannot validate the key", e)
-                            errorHandler.postException(e)
-                            proceedToSignUp()
-                        }
-                    }
-                }
             }
         } else {
             if (accountRepository.isVerifiedAccount) {
                 proceedToVisitsManagement(activity)
             } else {
+                proceedToSignUp()
+            }
+        }
+    }
+
+    private fun parseDeeplink(parameters: Map<String, Any>, activity: Activity) {
+        val publishableKey = parameters["publishable_key"] as String?
+        val driverId = parameters["driver_id"] as String?
+        val email = parameters["email"] as String?
+        val deeplink = parameters["~referring_link"] as String?
+        val deeplinkWithoutGetParams = deeplink.urlClearGetParams()
+        val phoneNumber = parameters["phone_number"] as String?
+        val metadata: Map<String, Any>? = when (val param = parameters["metadata"]) {
+            is String -> {
+                moshi.adapter<Map<String, Any>>(
+                    Types.newParameterizedType(
+                        Map::class.java, String::class.java,
+                        Any::class.java
+                    )
+                ).fromJson(param)
+            }
+            is Map<*, *> -> param as Map<String, Any>
+            else -> null
+        }
+
+        when {
+            publishableKey == null -> {
+                errorHandler.postText(
+                    osUtilsProvider.stringFromResource(
+                        R.string.splash_screen_invalid_link,
+                        osUtilsProvider.stringFromResource(R.string.splash_screen_no_key)
+                    )
+                )
+                crashReportsProvider.logException(InvalidDeeplinkException("publishableKey == null"))
+                proceedToSignUp()
+            }
+            email == null && phoneNumber == null && driverId == null -> {
+                errorHandler.postText(
+                    osUtilsProvider.stringFromResource(
+                        R.string.splash_screen_invalid_link,
+                        osUtilsProvider.stringFromResource(R.string.splash_screen_no_username)
+                    )
+                )
+                crashReportsProvider.logException(
+                    InvalidDeeplinkException(
+                        "email == null && phoneNumber == null && driverId == null"
+                    )
+                )
+                proceedToSignUp()
+            }
+            else -> {
+                validatePublishableKeyAndProceed(
+                    publishableKey = publishableKey,
+                    driverId = driverId,
+                    email = email,
+                    phoneNumber = phoneNumber,
+                    metadata = metadata,
+                    deeplinkWithoutGetParams = deeplinkWithoutGetParams,
+                    activity = activity
+                )
+            }
+        }
+    }
+
+    private fun validatePublishableKeyAndProceed(
+        publishableKey: String,
+        driverId: String?,
+        email: String?,
+        phoneNumber: String?,
+        metadata: Map<String, Any>?,
+        deeplinkWithoutGetParams: String?,
+        activity: Activity
+    ) {
+        loadingState.postValue(true)
+        viewModelScope.launch {
+            try {
+                val correctKey = accountRepository.onKeyReceived(publishableKey)
+                // Log.d(TAG, "onKeyReceived finished")
+                if (correctKey) {
+                    // Log.d(TAG, "Key validated successfully")
+                    if (driverId != null && (email == null && phoneNumber == null)) {
+                        errorHandler.postText(
+                            osUtilsProvider.stringFromResource(
+                                R.string.splash_screen_deprecated_link
+                            )
+                        )
+                        if (driverId.isEmail()) {
+                            driverRepository.setUserData(
+                                email = driverId,
+                                phoneNumber = phoneNumber,
+                                metadata = metadata,
+                                deeplinkWithoutGetParams = deeplinkWithoutGetParams
+                            )
+                        } else {
+                            driverRepository.setUserData(
+                                driverId = driverId,
+                                phoneNumber = phoneNumber,
+                                metadata = metadata,
+                                deeplinkWithoutGetParams = deeplinkWithoutGetParams
+                            )
+                        }
+                        proceedToVisitsManagement(activity)
+                    } else {
+                        driverRepository.setUserData(
+                            email = email,
+                            phoneNumber = phoneNumber,
+                            metadata = metadata,
+                            deeplinkWithoutGetParams = deeplinkWithoutGetParams
+                        )
+                        proceedToVisitsManagement(activity)
+                    }
+                } else {
+                    throw Exception("Invalid publishable_key")
+                }
+            } catch (e: Exception) {
+                errorHandler.postException(e)
+                crashReportsProvider.logException(InvalidDeeplinkException(e))
                 proceedToSignUp()
             }
         }
@@ -183,4 +212,10 @@ class SplashScreenViewModel(
     fun String?.urlClearGetParams(): String? {
         return this?.split("?")?.first()
     }
+
+    class InvalidDeeplinkException(m: String) : Exception(m) {
+        constructor(e: Exception) : this(e.format())
+    }
 }
+
+
