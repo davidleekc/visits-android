4140ad755f8fb1b8f3cdea59d1f4bd9c0af0b2ef
pavel-kuzntesov-5x <pavel.kuzntesov.5x@gmail.com>
2021-09-21 12:22:31 +0300

    Tests fixes

--- app/src/test/java/com/hypertrack/android/view_models/SelectDestinationViewModelTest.kt
+++ app/src/test/java/com/hypertrack/android/view_models/SelectDestinationViewModelTest.kt
@@ -3,10 +3,10 @@ package com.hypertrack.android.view_models
 import com.google.android.gms.maps.model.LatLng
 import com.hypertrack.android.api.MainCoroutineScopeRule
 import com.hypertrack.android.ui.common.select_destination.reducer.*
+import com.hypertrack.android.utils.False
 import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.test.TestCoroutineScope
 import org.junit.Rule
 import org.junit.Test
 
@@ -19,15 +19,20 @@ class SelectDestinationViewModelTest {
 
     @Test
     fun init() {
-        val reducer = SelectDestinationViewModelReducer(mockk(relaxed = true), TestCoroutineScope())
+        val reducer = SelectDestinationViewModelReducer()
 
         runBlocking {
-            var state: State = Initial(MapLocationState(MapNotReady, null, false), null)
+            var state: State = Initial(MapLocationState(MapNotReady, null, False), null)
             println(state)
-            state = reducer.sendAction(state, UserLocation(LatLng(37.4, -122.0)))
-            state =
-                reducer.sendAction(state, MapReadyAction(mockk(relaxed = true), LatLng(0.0, 0.0)))
-            state = reducer.sendAction(state, MapCameraMoved(LatLng(0.01, 0.0), false))
+            state = reducer.sendAction(state, UserLocation(LatLng(37.4, -122.0))).newState
+            state = reducer.sendAction(
+                state,
+                MapReadyAction(mockk(relaxed = true), LatLng(0.0, 0.0), "address")
+            ).newState
+            state = reducer.sendAction(
+                state,
+                MapCameraMoved(LatLng(0.01, 0.0), "address", false)
+            ).newState
         }
 
     }
@@ -35,11 +40,12 @@ class SelectDestinationViewModelTest {
     private fun SelectDestinationViewModelReducer.sendAction(
         oldState: State,
         action: Action
-    ): State {
-        println("action = ${action}")
-        val newState = reduceAction(oldState, action)
-        println("new state = ${newState}")
-        return newState
+    ): ReducerResult {
+        println("action = $action")
+        val res = reduceAction(oldState, action)
+        println("new state = ${res.newState}")
+        println("effects = ${res.effects}")
+        return res
     }
 
 }
